59411efd04fdd9c5768c0da0cfe4f5fb2b24a4d1
Cattimus <github_commit@cattimus.dev>
2024-03-02 13:59:43 -0800

    Created initial structs for file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct CrustFile {
+  extension_len: u8,
+  name_len: u16,
+  data_len: u32,
+  filename: String,
+  extension: String,
+  file_data: Vec<u8>
+}
+
+struct CrustPacked {
+  identifier: String,
+  file_count: u32,
+  files: Vec<CrustFile>
+}
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
 }
